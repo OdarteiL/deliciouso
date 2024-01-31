54f2705ce0d9be16fe6829274876c2daa887bb23
Odartei Lamptey <odarteilamptey.ml@gmail.com>
2024-01-31 21:41:36 +0000

    updated the menu page

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -2,7 +2,7 @@ function MenuPage() {
   return (
     <>
       <div>
-        <div className="w-[400px] h-[300px] bg-black relative rounded-[20px] my-8 mx-8 ">
+        <div className="md:w-[400px] md:h-[300px] bg-black relative rounded-[20px] my-8 mx-8 ">
           <div className="w-full h-[80%] object-cover shadow-lg rounded-[20px] ">
             <img src="https://res.cloudinary.com/dlcqoiyb1/image/upload/v1706729500/ecommerce/flat-lay-table-full-delicious-food-composition_sgkidf.jpg" alt="meat" className="w-full h-full rounded-t-lg " />
           </div>
